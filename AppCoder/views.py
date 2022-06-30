e3c6998d4e1d24efb8a25cdf7aea496401072f2a
EmilianoCelleri <emiliano.celleri@gmail.com>
2022-06-30 20:53:37 -0300

    Segundo commit seguro

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -11,3 +11,20 @@ def curso(self):
     curso.save()
     texto= f"Curso creado: {curso.nombre} {curso.comision}"
     return HttpResponse(texto)
+
+def inicio(request):
+    return render (request, "Appcoder/inicio.html")
+
+
+def cursos(request):
+    return render (request, "Appcoder/cursos.html")
+
+def profesores(request):
+    return render (request, "Appcoder/profesores.html")
+
+def estudiantes(request):
+    return render (request, "Appcoder/estudiantes.html")
+
+def entregables(request):
+    return render (request, "Appcoder/entregables.html")
+
